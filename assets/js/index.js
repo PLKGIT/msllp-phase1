b72b62c6a8ac6eaab047d9a8db7a93b3b3b7cf08
Lammie <plkelly@yahoo.com>
2020-04-25 15:03:04 -0700

    Minor copyedits.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,12 @@
-var arrImages = new Array('/assets/images/home1_header.png', '/assets/images/home2_header.png', '/assets/images/home3_header.png');
-var index = 1;
-
-function rotateImage() {
-    $('#myImage').fadeOut('fast', function () {
-        $(this).attr('src', arrImages[index]);
-        $(this).fadeIn('fast', function () {
-            if (index == arrImages.length - 1) {
-                index = 0;
-            }
-            else {
-                index++;
-            }
-        });
-    });
-}
-
 $(document).ready(function () {
-    setInterval(rotateImage, 20000);
+
+
+
+
+
+
+
+
+
+
 });
